e69a608fcb4a1c3c8a9bc8de3ccb5e22a0e4e756
xBigDaddyx <yussupcazzanova97@gmail.com>
2025-05-25 10:39:05 +0700

    initial commit config

---
+++ pkg/cache/redis.go
@@ -0,0 +1,49 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/Lumina-Enterprise-Solutions/prism-common-libs/pkg/config"
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClient struct {
+	client *redis.Client
+}
+
+func NewRedisClient(cfg config.RedisConfig) *RedisClient {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     cfg.Address(),
+		Password: cfg.Password,
+		DB:       cfg.DB,
+	})
+
+	return &RedisClient{client: rdb}
+}
+
+func (r *RedisClient) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	data, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	return r.client.Set(ctx, key, data, expiration).Err()
+}
+
+func (r *RedisClient) Get(ctx context.Context, key string, dest interface{}) error {
+	data, err := r.client.Get(ctx, key).Result()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal([]byte(data), dest)
+}
+
+func (r *RedisClient) Delete(ctx context.Context, key string) error {
+	return r.client.Del(ctx, key).Err()
+}
+
+func (r *RedisClient) Exists(ctx context.Context, key string) (bool, error) {
+	count, err := r.client.Exists(ctx, key).Result()
+	return count > 0, err
+}
---
+++ pkg/model/base.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"time"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type BaseModel struct {
+	ID        uuid.UUID      `json:"id" gorm:"type:uuid;primary_key;default:gen_random_uuid()"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+}
+
+type User struct {
+	BaseModel
+	Email        string `json:"email" gorm:"uniqueIndex"`
+	PasswordHash string `json:"-"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Status       string `json:"status" gorm:"default:active"`
+	Roles        []Role `json:"roles" gorm:"many2many:user_roles;"`
+}
+
+type Role struct {
+	BaseModel
+	Name        string                 `json:"name"`
+	Permissions map[string]interface{} `json:"permissions" gorm:"type:jsonb"`
+	Users       []User                 `json:"-" gorm:"many2many:user_roles;"`
+}
+
+type Tenant struct {
+	BaseModel
+	Name   string `json:"name"`
+	Slug   string `json:"slug" gorm:"uniqueIndex"`
+	Status string `json:"status" gorm:"default:active"`
+}
