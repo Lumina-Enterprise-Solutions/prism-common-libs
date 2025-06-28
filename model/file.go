df51d7e34cc973d2cedf4ae64436b3dadab6a026
Faisal Yusuf Nur Setyo <yussupcazzanova97@gmail.com>
2025-06-28 17:45:01 +0700

    Tambahan field Tags []string ke model.FileMetadata di prism-common-libs

--- model/file.go
+++ model/file.go
@@ -5,9 +5,10 @@ import "time"
 type FileMetadata struct {
 	ID           string    `json:"id"`
 	OriginalName string    `json:"original_name"`
-	StoragePath  string    `json:"-"` // Sembunyikan dari klien
+	StoragePath  string    `json:"-"`
 	MimeType     string    `json:"mime_type"`
 	SizeBytes    int64     `json:"size_bytes"`
 	OwnerUserID  *string   `json:"owner_user_id,omitempty"`
 	CreatedAt    time.Time `json:"created_at"`
+	Tags         []string  `json:"tags,omitempty"`
 }
