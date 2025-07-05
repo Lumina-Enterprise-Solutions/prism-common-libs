1c8a8b4d4b2e79902d5fc0aa51699bd0337ef911
Faisal Yusuf Nur Setyo <yussupcazzanova97@gmail.com>
2025-07-05 15:28:19 +0700

    Add Organization and Team models with JSON serialization

---
+++ model/team.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type Team struct {
+	ID             string    `json:"id"`
+	OrganizationID string    `json:"organization_id"`
+	Name           string    `json:"name"`
+	Department     *string   `json:"department,omitempty"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+}
